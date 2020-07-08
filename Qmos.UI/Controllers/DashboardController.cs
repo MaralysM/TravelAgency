88a84d50638768375636db418e9fd83768bb4487
maralys.molina <maralys.molina@key-core.com>
2020-07-08 18:12:21 -0400

    feat(): Refresh screen

--- Qmos.UI/Controllers/DashboardController.cs
+++ Qmos.UI/Controllers/DashboardController.cs
@@ -41,46 +41,46 @@ namespace Qmos.UI.Controllers
 
         public IActionResult MTDDelays()
         {
-            return View("MTDDelays");
+            return View("MTDDelays", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
 
         public IActionResult MTDTapTempand02PPM()
         {
-            return View("MTDTapTempand02PPM");
+            return View("MTDTapTempand02PPM", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
         public IActionResult KWhPerScrapTon()
         {
-            return View("KWhPerScrapTon");
+            return View("KWhPerScrapTon", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
 
         public IActionResult ScrapTonPerHour()
         {
-            return View("ScrapTonPerHour");
+            return View("ScrapTonPerHour", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
 
         public IActionResult IronYield()
         {
-            return View("IronYield");
+            return View("IronYield", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
         public IActionResult TargetPPM()
         {
-            return View("TargetPPM");
+            return View("TargetPPM", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
         public IActionResult TargetTemp()
         {
-            return View("TargetTemp");
+            return View("TargetTemp", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
         public IActionResult TapWtTarget()
         {
-            return View("TapWtTarget");
+            return View("TapWtTarget", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
         public IActionResult MTDProduction()
         {
-            return View("MTDProduction");
+            return View("MTDProduction", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
         public IActionResult MTDAverage()
         {
-            return View("MTDAverage");
+            return View("MTDAverage", new UpdateTimeViewModel { TIMEMILLISECONDS = Manager.ConversionToMilliseconds().Result });
         }
 
     }
